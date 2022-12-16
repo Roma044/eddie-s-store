5131f57be7f0071a9ba1fed8ff7e05f2ef041630
Roma044 <roma.bakalets0406@gmail.com>
2022-12-16 11:57:36 -0500

    added scroll-up on every page

--- js/scroll-up.js
+++ js/scroll-up.js
@@ -1,32 +1,25 @@
-// const offset = 100;
-// const scrollUp = document.querySelector(".scroll-up");
-// const scrollUpSvgPath = document.querySelector(".scroll-up___svg-path");
-// const pathLength = scrollUpSvgPath.getTotalLength();
+let mybutton = document.getElementById("myBtn");
 
-// scrollUpSvgPath.style.strokeDasharray = `${pathLength} ${pathLength}`;
-// scrollUpSvgPath.style.transition = `stroke-dashoffset 20ms`;
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
 
-// const getTop = () => window.Yoffset || document.documentElement.scrollTop;
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
 
-// const updateDashoffset = () => {
-//   const height = document.documentElement.scrollHeight - window.innerHeight;
-//   const dashoffset = pathLength - (getTop() * pathLength) / height;
-//   scrollUpSvgPath.style.strokeDashoffset = dashoffset;
-// };
-
-// window.addEventListener("scroll", () => {
-//   updateDashoffset();
-
-//   if (getTop() > offset) {
-//     scrollUp.classList.add("scroll-up--activity");
-//   } else {
-//     scrollUp.classList.remove("scroll-up--activity");
-//   }
-// });
-
-// scrollUp.addEventListener("click", () => {
-//   window.scrollTo({
-//     top: 0,
-//     behavior: "smooth",
-//   });
-// });
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+  window.scroll({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+}
